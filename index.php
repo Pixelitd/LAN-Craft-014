1d37483e29ddac5199b9db3e81be2866a455d7f6
Pixelitəd <111326476+Pixelitd@users.noreply.github.com>
2024-10-06 21:40:54 +0800

    update index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" meta />
+    <title>TITLE</title>
+    <style type="text/css" media="all">
+        body{
+            background-color: #6E4413;
+        }
+        h1{
+            color: #AEAEAE;
+        }
+    </style>
+</head>
+<body>
+    <h1>LANCraft014</h1>
+</body>
+</html>
